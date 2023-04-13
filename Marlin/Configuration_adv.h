2da7f906cb2454d3e2b85a6208c1a5f41695f748
Slash1001001 <slash1001@gmail.com>
2023-04-12 22:41:20 -0400

    Changing case fan to come on at boot

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -705,9 +705,9 @@
 /**
  * M355 Case Light on-off / brightness
  */
-//#define CASE_LIGHT_ENABLE
+#define CASE_LIGHT_ENABLE
 #if ENABLED(CASE_LIGHT_ENABLE)
-  //#define CASE_LIGHT_PIN 4                  // Override the default pin if needed
+  #define CASE_LIGHT_PIN LED3_PIN  //JT                // Override the default pin if needed
   #define INVERT_CASE_LIGHT false             // Set true if Case Light is ON when pin is LOW
   #define CASE_LIGHT_DEFAULT_ON true          // Set default power-up state on
   #define CASE_LIGHT_DEFAULT_BRIGHTNESS 255 //JT   // Set default power-up brightness (0-255, requires PWM pin)
